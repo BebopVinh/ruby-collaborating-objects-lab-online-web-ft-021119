f4be3e9a1460aa20704045ec506fc4d8f0ef1b97
Vinh Do <vinhdo07@gmail.com>
2019-02-26 20:17:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,7 +17,38 @@ class Artist
   end
 
   def save
+    @@all << selfrequire 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def save
     @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    @@all.find {|artist| artist.name == name} || self.new(name)
+  end
+
+  def print_songs
+    @songs.each {|song| puts song.name}
+  end
+end
   end
 
   def self.all
