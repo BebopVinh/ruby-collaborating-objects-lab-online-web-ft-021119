dd8e3b9e872913e799f4a018e13a86a34b2259bf
Vinh Do <vinhdo07@gmail.com>
2019-02-26 20:17:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,6 @@ class Song
     arr = file.split(" - ")
     temp = self.new(arr[1])
     @artist = Artist.new(arr[0])
-    temp.artist = Artist.new(arr[0])
     @artist.save
     @artist.add_song(temp)
     temp
