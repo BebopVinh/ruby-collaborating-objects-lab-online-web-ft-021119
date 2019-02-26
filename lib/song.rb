53791d9eeb149fe51bc0f90a09f64ca86b26ea70
Vinh Do <vinhdo07@gmail.com>
2019-02-26 20:35:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,7 @@ class Song
 
   def initialize(name)
     @name = name
+    @artist = nil
   end
 
   def artist=(artist)
@@ -14,8 +15,8 @@ class Song
   def self.new_by_filename(filename)
     array = filename.split(/[-.]/)
     song = self.new(array[1].strip)
+    binding.pry
     @artist = Artist.find_or_create_by_name(array[0].strip)
-    song.artist
     @artist.save
     @artist.add_song(song)
     song
